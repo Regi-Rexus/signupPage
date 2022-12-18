43e2638946a7c0ab84f25d9cfeb1e56c903016d0
Regina W <whyder.bagina@gmail.com>
2022-12-19 00:24:20 +0100

    tschanges

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import "./style.css";
-<<<<<<< HEAD
 // import axios from "axios";
 // const form = document.getElementById("form") as HTMLFormElement;
 // form.addEventListener("submit" async (event)=> {
@@ -15,19 +14,3 @@ import "./style.css";
 //     alert(`Successfully registered! Your user id is: ${user.data.id}`);
 
 // });
-=======
-import axios from "axios";
-export {};
-const form = document.getElementById("form") as HTMLFormElement;
-form.addEventListener("submit", async (event) => {
-	event.preventDefault();
-	const formData = new FormData(form);
-	const formEntries = formData.entries();
-	const data = Object.fromEntries(formData.entries());
-
-	const user = await axios.post("http://0.0.0.0:3000/users", data);
-	console.log(user.data);
-
-	alert(`Successfully registered! Your user id is: ${user.data.id}`);
-});
->>>>>>> e6c737d1afd57402175153aa2c957bc56b377c65
